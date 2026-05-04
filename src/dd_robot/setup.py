716dff9e89151a80116d3fbd8ea4b18f369413fb
Warren <wrkzk@protonmail.com>
2026-05-04 14:51:52 -0500

    Beginning creation of simple swarm robot

---
+++ src/dd_robot/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'dd_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world')))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='warren',
+    maintainer_email='wrkzk@protonmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
