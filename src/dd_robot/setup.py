64f42d39a63dae35f4b06e5cfe9cc29eddc58fb2
Warren <wrkzk@protonmail.com>
2026-05-12 22:47:29 -0500

    Look for .urdf.xacro files when building

--- src/dd_robot/setup.py
+++ src/dd_robot/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf.xacro'))),
         (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world')))
     ],
     install_requires=['setuptools'],
