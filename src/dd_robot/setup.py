f5c3caddd4fa61d02887f301a4a2788a8ab5dce7
Warren <wrkzk@protonmail.com>
2026-05-14 19:34:37 -0500

    Make dd_robot setup.py look for world files

--- src/dd_robot/setup.py
+++ src/dd_robot/setup.py
@@ -14,7 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
         (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*.urdf.xacro'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.world')))
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*.sdf')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
